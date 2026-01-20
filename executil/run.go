d1a8e500c082fe2b50d7c455c3bca47651931dda
pitanguinha <luigipereira1001@gmail.om>
2026-01-19 21:36:36 -0300

    feat: add helper for shell execution

--- executil/run.go
+++ executil/run.go
@@ -3,14 +3,18 @@ package executil
 import (
 	"context"
 	"os/exec"
+	"strings"
 )
 
-// ExecCommand executes a command in the given context and returns the command, its stdout and stderr pipes, and any error encountered during execution.
-func ExecCommand(ctx context.Context, command ...string) *exec.Cmd {
-	if len(command) == 0 {
+// Exec executes a command in the given context and returns the command, its stdout and stderr pipes, and any error encountered during execution.
+func Exec(ctx context.Context, name string, args ...string) *exec.Cmd {
+	return exec.CommandContext(ctx, name, args[0:]...)
+}
+
+func ExecShell(ctx context.Context, command string) *exec.Cmd {
+	if strings.TrimSpace(command) == "" {
 		return nil
 	}
-	cmd := exec.CommandContext(ctx, command[0], command[1:]...)
 
-	return cmd
+	return exec.CommandContext(ctx, "sh", "-c", command)
 }
